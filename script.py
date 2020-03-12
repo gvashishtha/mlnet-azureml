06ad96b48bebf3ae2e1da9b78e19b5071ae1e69b
Gopal K. Vashishtha <gopalv@microsoft.com>
2020-03-12 09:00:56 -0700

    adding so file

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import argparse
+import os
 import subprocess
+
+from azureml.core import Run
 
 def main():
     
@@ -8,14 +11,24 @@ def main():
         help='path to exe')
     parser.add_argument('--data_file', type=str, default=None,
         help='path to data file')
-    parser.add_argument('--output_path', type=str, default=None,
+    parser.add_argument('--output_dir', type=str, default=None,
         help='path to store your model')
+    parser.add_argument('--model_file', type=str, default=None,
+        help='path to model file')
 
     args = parser.parse_args()
     FNULL = open(os.devnull, 'w')
     exe = args.exe
-    filename = args.data_file
-    output = args.output_path
-    args = exe + " " + filename + " " + output
+    data_file = args.data_file
+    output_dir = args.output_dir
+    model_file = args.model_file
 
-    subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=False)
+    print('cur dir is {}, containing {}'.format(os.getcwd(), os.listdir(os.getcwd())))
+    os.makedirs(name=output_dir, exist_ok=True)
+
+    args = [exe, data_file, os.path.join(output_dir, model_file)]
+    
+    subprocess.run(args, stdout=FNULL, stderr=FNULL, shell=False)
+
+if (__name__=='__main__'):
+    main()
